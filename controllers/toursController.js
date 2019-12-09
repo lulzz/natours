e742abe24166277ea5df4617c5643a3dc1dce2b9
lulzz <dzevad.del@gmail.com>
2019-12-09 11:30:11 +0100

    added multiple file and image upload to website

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,3 +1,6 @@
+const multer = require('multer');
+const sharp = require('sharp');
+
 const Tour = require('./../models/toursModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
@@ -9,6 +12,56 @@ const {
    getOne,
    getAll
 } = require('./handlerFactory');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+   if (file.mimetype.startsWith('image')) {
+      cb(null, true);
+   } else {
+      cb(new AppError('Not an image! Please upload only images.', 400), false);
+   }
+};
+
+const upload = multer({
+   storage: multerStorage,
+   fileFilter: multerFilter
+});
+
+exports.uploadTourImages = upload.fields([
+   { name: 'imageCover', maxCount: 1 },
+   { name: 'images', maxCount: 3 }
+]);
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+   if (!req.files.imageCover || !req.files.images) return next();
+
+   // 1. cover image
+   req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+   await sharp(req.files.imageCover[0].buffer)
+      .resize(2000, 1333)
+      .toFormat('jpeg')
+      .jpeg({ qaulity: 90 })
+      .toFile(`public/img/tours/${req.body.imageCover}`);
+
+   // 2. images
+   req.body.images = [];
+   await Promise.all(
+      req.files.images.map(async (file, index) => {
+         const filename = `tour-${req.params.id}-${Date.now()}-${index +
+            1}.jpeg`;
+
+         await sharp(file.buffer)
+            .resize(2000, 1333)
+            .toFormat('jpeg')
+            .jpeg({ qaulity: 90 })
+            .toFile(`public/img/tours/${filename}`);
+         req.body.images.push(filename);
+      })
+   );
+
+   next();
+});
 
 exports.getTop5Tours = (req, res, next) => {
    req.query.limit = '5';
--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 
 const {
-    getAllTours,
-    createTour,
-    getTour,
-    updateTour,
-    deleteTour,
-    getTop5Tours,
-    getTourStatistics,
-    getMonthlyPlan,
-    getToursWithin,
-    getDistances
+   getAllTours,
+   createTour,
+   getTour,
+   updateTour,
+   deleteTour,
+   getTop5Tours,
+   getTourStatistics,
+   getMonthlyPlan,
+   getToursWithin,
+   getDistances,
+   resizeTourImages,
+   uploadTourImages
 } = require('./../controllers/toursController');
 
 const reviewRouter = require('./../routes/reviewRoutes');
@@ -22,25 +24,31 @@ const router = express.Router();
 router.route('/top-5-cheap').get(getTop5Tours, getAllTours);
 router.route('/tour-stats').get(getTourStatistics);
 router
-    .route('/monthly-plan/:year')
-    .get(protect, restrictTo('admin', 'lead-guide', 'guide'), getMonthlyPlan);
+   .route('/monthly-plan/:year')
+   .get(protect, restrictTo('admin', 'lead-guide', 'guide'), getMonthlyPlan);
 
 router
-    .route('/tours-within/:distance/center/:latlng/unit/:unit')
-    .get(getToursWithin);
+   .route('/tours-within/:distance/center/:latlng/unit/:unit')
+   .get(getToursWithin);
 
 router.route('/distances/:latlng/unit/:unit').get(getDistances);
 
 router
-    .route('/')
-    .get(getAllTours)
-    .post(protect, restrictTo('admin', 'lead-guide'), createTour);
+   .route('/')
+   .get(getAllTours)
+   .post(protect, restrictTo('admin', 'lead-guide'), createTour);
 
 router
-    .route('/:id')
-    .get(getTour)
-    .patch(protect, restrictTo('admin', 'lead-guide'), updateTour)
-    .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+   .route('/:id')
+   .get(getTour)
+   .patch(
+      protect,
+      restrictTo('admin', 'lead-guide'),
+      uploadTourImages,
+      resizeTourImages,
+      updateTour
+   )
+   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 router.use('/:tourId/reviews', reviewRouter);
 
