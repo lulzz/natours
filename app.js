e5c00a494a04ead0cad1bc0322307a7ed8b9a1df
lulzz <dzevad.del@gmail.com>
2019-12-11 18:21:37 +0100

    Implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+// cross origin
+// Access-Control-Allow-Origin
+app.use(cors());
+
+// http method
+app.options('*', cors());
 
 // Set security HTTP headers
 app.use(helmet());
