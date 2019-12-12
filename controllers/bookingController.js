7749c9d16c0a3add79abbb3912b2f38ba8325377
lulzz <dzevad.del@gmail.com>
2019-12-12 10:12:36 +0100

    stripe errors corrected

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -25,11 +25,15 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       client_reference_id: req.params.tourID,
 
       //   information about product user want it to purchase
-      line_items: [
+      display_items: [
          {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [
+               `${req.protocol}://${req.get('host')}/img/tours/${
+                  tour.imageCover
+               }`
+            ],
             amount: tour.price * 100,
             currency: 'usd',
             quantity: 1
@@ -47,7 +51,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 exports.createBookingCheckout = async session => {
    const tour = session.client_reference_id;
    const user = (await User.findOne({ email: session.customer_email })).id;
-   const price = session.line_items[0].amount / 100;
+   const price = session.display_items[0].amount / 100;
    await Booking.create({ tour, user, price });
 };
 
@@ -65,7 +69,7 @@ exports.webhookCheckout = (req, res, next) => {
       res.status(400).send(`Webhook error: ${err.message}`);
    }
 
-   if (event.type === 'checkout.session.complete') {
+   if (event.type === 'checkout.session.completed') {
       this.createBookingCheckout(event.data.object);
    }
 
